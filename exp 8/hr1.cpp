11f21356cc8c9a41e143cf7c5dc82a0fa420b636
sivasankar10 <111382075+sivasankar10@users.noreply.github.com>
2023-08-23 09:06:32 +0530

    Add files via upload

---
+++ exp 8/hr1.cpp
@@ -0,0 +1,26 @@
+#include "hr1.h"
+
+// Constructors/Destructors
+//  
+
+hr1::hr1 () {
+initAttributes();
+}
+
+hr1::~hr1 () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void hr1::initAttributes () {
+}
+
---
+++ exp 8/hr1.h
@@ -0,0 +1,130 @@
+
+#ifndef HR1_H
+#define HR1_H
+
+#include <string>
+
+/**
+  * class hr1
+  * 
+  */
+
+class hr1
+{
+public:
+
+  // Constructors/Destructors
+  //  
+
+
+  /**
+   * Empty Constructor
+   */
+  hr1 ();
+
+  /**
+   * Empty Destructor
+   */
+  virtual ~hr1 ();
+
+  // Static Public attributes
+  //  
+
+  // Public attributes
+  //  
+
+
+  // Public attribute accessor methods
+  //  
+
+
+  // Public attribute accessor methods
+  //  
+
+
+
+  /**
+   */
+  void loss_of_pay ()
+  {
+  }
+
+
+  /**
+   */
+  void tax ()
+  {
+  }
+
+protected:
+
+  // Static Protected attributes
+  //  
+
+  // Protected attributes
+  //  
+
+public:
+
+
+  // Protected attribute accessor methods
+  //  
+
+protected:
+
+public:
+
+
+  // Protected attribute accessor methods
+  //  
+
+protected:
+
+
+private:
+
+  // Static Private attributes
+  //  
+
+  // Private attributes
+  //  
+
+  void check_details;
+public:
+
+
+  // Private attribute accessor methods
+  //  
+
+private:
+
+public:
+
+
+  // Private attribute accessor methods
+  //  
+
+
+  /**
+   * Set the value of check_details
+   * @param new_var the new value of check_details
+   */
+  void setCheck_details (void new_var)   {
+      check_details = new_var;
+  }
+
+  /**
+   * Get the value of check_details
+   * @return the value of check_details
+   */
+  void getCheck_details ()   {
+    return check_details;
+  }
+private:
+
+
+  void initAttributes () ;
+
+};
+
+#endif // HR1_H

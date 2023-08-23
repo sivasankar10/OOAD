2c4a542ef2eaeeae82eb47055cb57bf3dd99a6e5
sivasankar10 <111382075+sivasankar10@users.noreply.github.com>
2023-08-23 09:45:10 +0530

    Add files via upload

---
+++ blood.cpp
@@ -0,0 +1,26 @@
+#include "blood.h"
+
+// Constructors/Destructors
+//  
+
+blood::blood () {
+initAttributes();
+}
+
+blood::~blood () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void blood::initAttributes () {
+}
+
---
+++ donor.cpp
@@ -0,0 +1,26 @@
+#include "donor.h"
+
+// Constructors/Destructors
+//  
+
+donor::donor () {
+initAttributes();
+}
+
+donor::~donor () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void donor::initAttributes () {
+}
+

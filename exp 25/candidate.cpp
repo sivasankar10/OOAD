c5b72a5c6a584842c11707d38639259d65d03e37
sivasankar10 <111382075+sivasankar10@users.noreply.github.com>
2023-08-23 13:31:51 +0530

    Add files via upload

---
+++ exp 25/candidate.cpp
@@ -0,0 +1,26 @@
+#include "candidate.h"
+
+// Constructors/Destructors
+//  
+
+candidate::candidate () {
+initAttributes();
+}
+
+candidate::~candidate () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void candidate::initAttributes () {
+}
+

dc7aa66b04da6ec11d98a904fb86bc353df1f36e
sivasankar10 <111382075+sivasankar10@users.noreply.github.com>
2023-08-23 09:30:11 +0530

    Add files via upload

---
+++ exp 12/trading_management_system.cpp
@@ -0,0 +1,26 @@
+#include "trading_management_system.h"
+
+// Constructors/Destructors
+//  
+
+trading_management_system::trading_management_system () {
+initAttributes();
+}
+
+trading_management_system::~trading_management_system () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void trading_management_system::initAttributes () {
+}
+

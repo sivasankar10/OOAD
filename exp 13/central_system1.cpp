cccd79eaf35d615dcd731eafab7ddb9b1ac39c79
sivasankar10 <111382075+sivasankar10@users.noreply.github.com>
2023-08-23 09:34:55 +0530

    Add files via upload

---
+++ exp 13/central_system1.cpp
@@ -0,0 +1,26 @@
+#include "central_system1.h"
+
+// Constructors/Destructors
+//  
+
+central_system1::central_system1 () {
+initAttributes();
+}
+
+central_system1::~central_system1 () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void central_system1::initAttributes () {
+}
+
